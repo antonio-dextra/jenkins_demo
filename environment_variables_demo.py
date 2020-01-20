7fdb66c46e605ffca83c400ed64f016c23993086
antonio-dextra <60110323+antonio-dextra@users.noreply.github.com>
2020-01-20 11:56:22 -0600

    Update environment_variables_demo.py

--- environment_variables_demo.py
+++ environment_variables_demo.py
@@ -5,6 +5,11 @@ def get_environment_variable_list():
     environment_variable_list = os.environ.keys()
     return environment_variable_list
 
+def get_key_value(key):
+    value = os.environ.get(key=key)
+    return value
+
 
 if __name__ == "__main__":
     print get_environment_variable_list()
+    print get_key_value('COMPUTERNAME')
